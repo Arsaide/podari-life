499f55b632ec16c5f7c39149493da0824afe8fe8
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-11-28 15:28:21 +0200

    Fixed deploy bugs

---
+++ src/app/give-help/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GiveHelp = () => {
+	return (
+		<div>
+			4213123123
+		</div>
+	)
+}
+
+export default GiveHelp
---
+++ src/app/what-we-do/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WhatWeDo = () => {
+	return (
+		<div>
+			123123123
+		</div>
+	)
+}
+
+export default WhatWeDo
