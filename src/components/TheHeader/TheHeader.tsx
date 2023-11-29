b66846322e9f2e2f5ba1f9bc15e66f432685e765
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-11-30 01:20:00 +0200

    Adaptive burger menu + completed work with the "Header" component

--- src/components/TheHeader/TheHeader.tsx
+++ src/components/TheHeader/TheHeader.tsx
@@ -1,9 +1,55 @@
+'use client'
 import styles from './TheHeader.module.scss'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import classNames from 'classnames'
 
-const TheHeader = () => {
+interface BurgerMenuProps {}
+
+const TheHeader: React.FC<BurgerMenuProps> = () => {
+	const [menuOpen, setMenuOpen] = useState(false)
+
+	const handleToggle = () => {
+		setMenuOpen(!menuOpen)
+	}
+
+	useEffect(() => {
+		const handleOutsideClick = (event: MouseEvent) => {
+			const menuBody = document.querySelector(`.${styles.menu__body}`)
+
+			if(menuOpen && !(event.target as HTMLElement).closest(`.${styles.menu__body}`)) {
+				setMenuOpen(false)
+			}
+		}
+
+		const handleEscKey = (event: KeyboardEvent) => {
+			if(menuOpen && event.key === 'Escape') {
+				setMenuOpen(false)
+			}
+		}
+
+		const disableScroll = () => {
+			if(menuOpen) {
+				document.body.style.overflow = 'hidden';
+			} else {
+				document.body.style.overflow = '';
+			}
+		}
+
+		document.addEventListener('click', handleOutsideClick);
+		document.addEventListener('keydown', handleEscKey);
+		disableScroll();
+
+		return () => {
+			document.removeEventListener('click', handleOutsideClick)
+			document.removeEventListener('keydown', handleEscKey)
+			document.body.style.overflow = ''
+		}
+	}, [menuOpen]);
+
+
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.header__container}>
@@ -24,18 +70,16 @@ const TheHeader = () => {
 						</div>
 					</div>
 					<div className={styles.header__rightContent}>
-						<div className={styles.header__menu}>
-							<button type='button' className={styles.icon__menu}>
+
+
+						<div className={classNames(styles.header__menu, styles.menu)}>
+							<button
+								type='button'
+								onClick={handleToggle}
+								className={classNames(styles.menu__icon, styles.icon__menu, menuOpen && styles.active)}>
 								<span></span>
 								<p className={styles.icon__text}>Меню</p>
 							</button>
-							<nav className={styles.menu__body}>
-								<ul className={styles.menu__list}>
-									<li className={styles.menu__item}><Link href='/who-we'>Кто мы</Link></li>
-									<li className={styles.menu__item}><Link href='/what-we-do'>Что мы делаем</Link></li>
-									<li className={styles.menu__item}><Link href='/give-help'>Помочь фонду</Link></li>
-								</ul>
-							</nav>
 						</div>
 						<Link className={styles.help} href='/give-help'>
 							Помочь
@@ -43,6 +87,34 @@ const TheHeader = () => {
 					</div>
 				</div>
 			</div>
+			<nav className={classNames(styles.menu__body, menuOpen && styles.active)}>
+				<ul className={styles.menu__list}>
+					<li className={styles.menu__item}>
+						<Link
+							className={styles.menu__link}
+							href='/who-we'
+							onClick={handleToggle}>
+							Кто мы
+						</Link>
+					</li>
+					<li className={styles.menu__item}>
+						<Link
+							className={styles.menu__link}
+							href='/what-we-do'
+							onClick={handleToggle}>
+							Что мы делаем
+						</Link>
+					</li>
+					<li className={styles.menu__item}>
+						<Link
+							className={styles.menu__link}
+							href='/give-help'
+							onClick={handleToggle}>
+							Помочь фонду
+						</Link>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	)
 }
