0324e9b741aec9519ad93f3c1ad3b14cdce5efa2
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-12-28 00:22:28 +0200

    Work with local DB

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import styles from './page.module.scss'
 import {Metadata} from "next";
+import posts from "../../../public/childrenPosts.json";
+
+async function getData(id: string) {
+    const post = posts.find((post) => post.id === Number(id))
+    return post || null;
+}
 
 type Props = {
     params: {
@@ -13,12 +20,27 @@ export async function generateMetadata({ params: { id } }: Props): Promise<Metad
     }
 }
 
-const Post = ({params: {id}} : Props) => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Post = async ({params: {id}}: Props) => {
+    const post = await getData(id)
+
+    if (!post) {
+        return (
+            <section className={styles.post}>
+                <div>
+                   <p>Post not found</p>
+                </div>
+            </section>
+        )
+    } else {
+        return (
+            <section className={styles.post}>
+                <div>
+                    <h3>{post.title}</h3>
+                    <p>{post.desc}</p>
+                </div>
+            </section>
+        )
+    }
 };
 
 export default Post;
