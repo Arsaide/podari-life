60c8c9cf394e47502ead9c3371b90bd524a78c43
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-12-29 23:43:59 +0200

    Update DB

---
+++ public/childList.ts
@@ -0,0 +1,53 @@
+interface ChildList {
+    src: string
+    alt: string
+    name: string
+    years: string
+    money: string
+    diagnosis: string
+}
+
+const childList: ChildList[] = [
+    {
+        src: '/children/preview_image3.webp',
+        name: 'Степа Гаврилюк',
+        alt: 'Изображение Степы Гаврилюка, 5 лет, острый лимфобластный лейкоз',
+        years: '5 лет',
+        money: '1 129 646',
+        diagnosis: 'Острый лимфобластный лейкоз',
+    },
+    {
+        src: '/children/preview_image2.webp',
+        alt: 'Изображение Фирдавса Шукурзода, 4 года, острый лимфобластный лейкоз',
+        name: 'Фирдавс Шукурзода',
+        years: '4 года',
+        money: '1 129 646',
+        diagnosis: 'Острый лимфобластный лейкоз',
+    },
+    {
+        src: '/children/preview_image1.webp',
+        alt: 'Изображение Саши Яковлева, 6 лет, первичный иммунодефицит',
+        name: 'Саша Яковлев',
+        years: '6 лет',
+        money: '819 255',
+        diagnosis: 'Первичный иммунодефицит',
+    },
+    {
+        src: '/children/preview_image5.webp',
+        alt: 'Изображение Полины Глазины, 14 лет, апластическая анемия',
+        name: 'Полина Глызина',
+        years: '14 лет',
+        money: '312 000',
+        diagnosis: 'Апластическая анемия',
+    },
+    {
+        src: '/children/preview_image4.webp',
+        alt: 'Изображение Ярослава Кораблина, острый лимфобластный лейкоз',
+        name: 'Ярослав Кораблин',
+        years: '3 года',
+        money: '1 129 646',
+        diagnosis: 'Острый лимфобластный лейкоз',
+    },
+];
+
+export default childList;
--- src/components/Pages/Home/Children/Children.tsx
+++ src/components/Pages/Home/Children/Children.tsx
@@ -2,19 +2,11 @@
 import React, {useEffect, useState} from 'react';
 import styles from './Children.module.scss'
 import Image from "next/image";
-import {getDebugPort} from "next/dist/server/lib/utils";
 import Link from "next/link";
-
-interface ChildImage {
-    src: string
-    alt: string
-    name: string
-    years: string
-    money: string
-    diagnosis: string
-}
+import childImages from '../../../../../public/childList'
 
 const Children = () => {
+
     const [hoveredChild, setHoveredChild] = useState<number | null>(null)
     const [isWidthScreen, setIsWidthScreen] = useState<boolean>(false)
 
@@ -29,49 +21,6 @@ const Children = () => {
             window.removeEventListener('resize', handleResize)
         };
     }, []);
-    
-    const childImages: ChildImage[] = [
-        {
-            src: '/children/preview_image3.webp',
-            name: 'Степа Гаврилюк',
-            alt: 'Изображение Степы Гаврилюка, 5 лет, острый лимфобластный лейкоз',
-            years: '5 лет',
-            money: '1 129 646',
-            diagnosis: 'Острый лимфобластный лейкоз',
-        },
-        {
-            src: '/children/preview_image2.webp',
-            alt: 'Изображение Фирдавса Шукурзода, 4 года, острый лимфобластный лейкоз',
-            name: 'Фирдавс Шукурзода',
-            years: '4 года',
-            money: '1 129 646',
-            diagnosis: 'Острый лимфобластный лейкоз',
-        },
-        {
-            src: '/children/preview_image1.webp',
-            alt: 'Изображение Саши Яковлева, 6 лет, первичный иммунодефицит',
-            name: 'Саша Яковлев',
-            years: '6 лет',
-            money: '819 255',
-            diagnosis: 'Первичный иммунодефицит',
-        },
-        {
-            src: '/children/preview_image5.webp',
-            alt: 'Изображение Полины Глазины, 14 лет, апластическая анемия',
-            name: 'Полина Глызина',
-            years: '14 лет',
-            money: '312 000',
-            diagnosis: 'Апластическая анемия',
-        },
-        {
-            src: '/children/preview_image4.webp',
-            alt: 'Изображение Ярослава Кораблина, острый лимфобластный лейкоз',
-            name: 'Ярослав Кораблин',
-            years: '3 года',
-            money: '1 129 646',
-            diagnosis: 'Острый лимфобластный лейкоз',
-        },
-    ];
 
     const handleChildHover = (index: number | null) => {
         setHoveredChild(isWidthScreen ? index : null)
