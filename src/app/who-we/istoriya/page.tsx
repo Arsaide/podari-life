b1bd3bba38f5a8b847813580cc96f75e77706486
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-02 23:31:54 +0200

    info page

--- src/app/who-we/istoriya/page.tsx
+++ src/app/who-we/istoriya/page.tsx
@@ -26,16 +26,100 @@ const Istoriya = () => {
                             <li className={styles.list__item}>оказание социальной и психологической помощи больным детям;</li>
                             <li className={styles.list__item}>содействие работе волонтерских групп при детских онкологических клиниках.</li>
                         </ul>
-                        <div className={styles.link}>
-                            <Link href='/who-we/kontakty-i-rekvizity'>
-                                <div className={styles.link__content}>
-                                    <div>
-                                        <span className={styles.link__title}>Контакты и реквизиты фонда</span>
-                                        <span className={styles.link__desc}>Здесь можно узнать адреса и телефоны фонда, а также банковские реквизиты</span>
+                        <div className={styles.links}>
+                            <div className={styles.link}>
+                                <Link href='/who-we/kontakty-i-rekvizity'>
+                                    <div className={styles.link__content}>
+                                        <div>
+                                            <span className={styles.link__title}>Контакты и реквизиты фонда</span>
+                                            <span className={styles.link__desc}>Здесь можно узнать адреса и телефоны фонда, а также банковские реквизиты</span>
+                                        </div>
+                                        <div className={styles.link__btn}>→</div>
                                     </div>
-                                    <div className={styles.link__btn}>→</div>
+                                </Link>
+                            </div>
+                            <h3 className={styles.files__title}>Учредительные документы фонда</h3>
+                            <div className={styles.files}>
+                                <div className={styles.filre__links}>
+                                    <div className={styles.download}>
+                                        <Link href='https://api.podari-zhizn.ru/storage/content_block/document/9/document-60602a7339dcccbe4b069ea462417fc0.pdf'>
+                                            <div className={styles.download__content}>
+                                                <div>
+                                                    <span className={styles.download__title}>Свидетельство о государственной регистрации</span>
+                                                    <span className={styles.download__desc}>Adobe Portable Document Format (851,5 КБ)</span>
+                                                </div>
+                                                <div className={styles.download__btn}>↓</div>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                    <div className={styles.download}>
+                                        <Link href='https://api.podari-zhizn.ru/storage/content_block/document/10/document-0761b3110a355fbe3a916d72054ea833.pdf'>
+                                            <div className={styles.download__content}>
+                                                <div>
+                                                    <span className={styles.download__title}>Свидетельство о постановке на учет в налоговом органе</span>
+                                                    <span className={styles.download__desc}>Adobe Portable Document Format (181,0 КБ)</span>
+                                                </div>
+                                                <div className={styles.download__btn}>↓</div>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                    <div className={styles.download}>
+                                        <Link href='https://api.podari-zhizn.ru/storage/content_block/document/11/document-1e69bee5a24c1cce78787598e6f44448.pdf'>
+                                            <div className={styles.download__content}>
+                                                <div>
+                                                    <span className={styles.download__title}>Свидетельство о внесении в реестр благотворительных организаций г. Москвы</span>
+                                                    <span className={styles.download__desc}>Adobe Portable Document Format (356,4 КБ)</span>
+                                                </div>
+                                                <div className={styles.download__btn}>↓</div>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                    <div className={styles.download}>
+                                        <Link href='https://api.podari-zhizn.ru/storage/content_block/document/235/document-1c6d034f1a4e5d89b57a443ad0da6b20.pdf'>
+                                            <div className={styles.download__content}>
+                                                <div>
+                                                    <span className={styles.download__title}>Свидетельство о регистрации на товарный знак «Подари жизнь»</span>
+                                                    <span className={styles.download__desc}>Adobe Portable Document Format (2,1 МБ)</span>
+                                                </div>
+                                                <div className={styles.download__btn}>↓</div>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                    <div className={styles.download}>
+                                        <Link href='https://api.podari-zhizn.ru/storage/content_block/document/236/document-c72fe258d494e3dcac39e3d0d34949b1.pdf'>
+                                            <div className={styles.download__content}>
+                                                <div>
+                                                    <span className={styles.download__title}>Сводная ведомость результатов проведения СОУТ</span>
+                                                    <span className={styles.download__desc}>Adobe Portable Document Format (1,1 МБ)</span>
+                                                </div>
+                                                <div className={styles.download__btn}>↓</div>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                    <div className={styles.download}>
+                                        <Link href='https://api.podari-zhizn.ru/storage/content_block/document/512/document-857a90c16dfb25ef256242342aa02318.pdf'>
+                                            <div className={styles.download__content}>
+                                                <div>
+                                                    <span className={styles.download__title}>Перечень рекомендуемых мероприятий по улучшению УТ</span>
+                                                    <span className={styles.download__desc}>Adobe Portable Document Format (164,1 КБ)</span>
+                                                </div>
+                                                <div className={styles.download__btn}>↓</div>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                    <div className={styles.download}>
+                                        <Link href='https://api.podari-zhizn.ru/storage/content_block/document/511/document-d04e25266348a97dcc6481696ac33d30.pdf'>
+                                            <div className={styles.download__content}>
+                                                <div>
+                                                    <span className={styles.download__title}>Контакты и реквизиты фонда</span>
+                                                    <span className={styles.download__desc}>Здесь можно узнать адреса и телефоны фонда, а также банковские реквизиты</span>
+                                                </div>
+                                                <div className={styles.download__btn}>↓</div>
+                                            </div>
+                                        </Link>
+                                    </div>
                                 </div>
-                            </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
