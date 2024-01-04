b157a25b0b095389d7cf319df6eec8416bd86277
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-05 00:25:18 +0200

    Added Give-Help page + styles

--- src/app/give-help/page.tsx
+++ src/app/give-help/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from "./page.module.scss"
+import TheHeadBannerProps from "@/components/TheHeadBannerProps/TheHeadBannerProps";
 const GiveHelp = () => {
 	return (
-		<div>
-			4213123123
-		</div>
+		<section>
+			<TheHeadBannerProps title={'Cделать пожертвование'} img={'help/banner.jpg'} text={'Мы делаем все, чтобы помогать было легко и удобно'}/>
+		</section>
 	)
 }
 
